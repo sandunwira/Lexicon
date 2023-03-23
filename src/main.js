b29a0dc41cfca571d03f7f6ab26c0c1ce08f8e5d
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-03-23 12:36:42 +0530

    Setup basic app

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { invoke } from '@tauri-apps/api/tauri'
+const invoke = window.__TAURI__.invoke
+
+// CONFIGURING SPLASHSCREEN START ===================================== //
+document.addEventListener('DOMContentLoaded', () => {
+  invoke('close_splashscreen')
+})
+// ======================================= CONFIGURING SPLASHSCREEN END //
