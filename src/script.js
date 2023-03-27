319bc7aa47bbc180e7fdb387e9bbcb4e14ef9b7f
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-03-27 18:30:34 +0530

    Updated icons and changed some codes to show json

--- src/script.js
+++ src/script.js
@@ -1,5 +1,27 @@
-// const noContext = document.getElementById('noMenu');
+// Get the form and input elements
+let form = document.getElementById("search-form");
+let input = document.getElementById("search-input");
 
-// noContext.addEventListener('contextmenu', (e) => {
-//   e.preventDefault();
-// });
+// Add event listener to the form submission
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // prevent the form from submitting
+
+  // Get the user-submitted word and create the URL for the JSON file
+  let word = input.value;
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+  // Use fetch to get the JSON data
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // Get the container element
+      let container = document.getElementById("data-container");
+      let htmlData = "";
+      // Loop through the data array
+      for (let i = 0; i < data.length; i++) {
+        htmlData += data[i].word;
+      }
+      // Insert the HTML into the container
+      container.innerHTML = htmlData;
+    });
+});
